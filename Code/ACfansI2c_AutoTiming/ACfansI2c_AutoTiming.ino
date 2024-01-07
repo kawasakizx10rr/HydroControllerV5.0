3573166baba1a21ae8c55aa6e1b9ca1d09edc8bd
Edwin Martin <husqvarna1990@yahoo.com>
2024-01-07 22:08:14 +0000

    Add files via upload

--- Code/ACfansI2c_AutoTiming/ACfansI2c_AutoTiming.ino
+++ Code/ACfansI2c_AutoTiming/ACfansI2c_AutoTiming.ino
@@ -3,12 +3,6 @@
   required to accurately chop up the AC wave form and read i2c commands. The loop
   time must be less than 83-100uS at any given time, currently 18uS is the max loop
   time with all functions running, as a result port manipulation is used throughout.
-  --------------------------------------------------------------------------
-  SwitchCases:
-  Switch     Value range     Modes                                  Example
-  Z          0 > 100         FAN_ONE_SPEED_IN_PERCENT               Z50
-  X          0 > 100         FAN_TWO_SPEED_IN_PERCENT               X75
-  --------------------------------------------------------------------------
   PINS:
   AC interruptor  = PD2 / PIN 2
   fanOne          = PD3 / PIN 3
@@ -24,6 +18,8 @@ volatile bool getHalfWaveDuration = true;
 volatile unsigned long fanOneTiming = 0, fanTwoTiming = 0;
 volatile unsigned long fanOneCutOffPoint = 0, fanTwoCutOffPoint = 0;
 volatile bool finishFanOneCutOff = false, finishFanTwoCutOff = false;
+volatile byte fanOneSpeed = 0;
+volatile byte fanTwoSpeed = 0;
 
 void setup() {
   //Serial.begin(115200); // Just for debugging, DO NOT use outside testing it will slow the code down!
@@ -40,44 +36,69 @@ void loop() {
 }
 
 void i2cReceiveEvent(const int cnt) {
-  static char switchCase = 0;
-  static byte value = 0;
   if (cnt == 2) {
-    switchCase = Wire.read();
-    value = Wire.read();
-  }
-  else if (switchCase == 'Z') {
-    if (value == 0) { // user fan 1 speed is 0% so turn off fan 1 constantly
+    fanOneSpeed = Wire.read();
+    fanTwoSpeed = Wire.read();
+     // user fan 1 speed is 0% so turn off fan 1 constantly
+    if (fanOneSpeed == 0) {
       PORTD &= ~_BV(PD3); // pin 3 low
       fanOneTiming = 0;
       finishFanOneCutOff = false;
       //Serial.println(F("f1 off"));
     }
-    else if (value == 100) { // user fan 1 speed is 100% so turn on fan 1 constantly
+     // user fan 1 speed is 100% so turn on fan 1 constantly
+    else if (fanOneSpeed == 100) {
       PORTD |= _BV(PD3); // pin 3 high
       fanOneTiming = 0;
       //Serial.println(F("f1 on"));
     }
-    else { // else fan 1 speed is 1% to 99% so set the fan 1 speed timing
-      fanOneTiming = (100 - value) * halfWaveDuration; // map speed to max half sine wave duration in uS
+    // else fan 1 speed is 1% to 99% so set the fan 1 speed timing
+    else {
+      // map speed to max half sine wave duration in uS
+      fanOneTiming = (100 - fanOneSpeed) * halfWaveDuration;
       //Serial.print(F("f1 uS:")); Serial.println(fanOneTiming);
     }
-  }
-  else if (switchCase == 'X') {
-    if (value == 0) { // user fan 2 speed is 0% so turn off fan 2 constantly
+    // user fan 2 speed is 0% so turn off fan 2 constantly
+    if (fanTwoSpeed == 0) { 
       PORTD &= ~_BV(PD4); // pin 4 low
       fanTwoTiming = 0;
       finishFanTwoCutOff = false;
       //Serial.println(F("f2 off"));
     }
-    else if (value == 100) { // user fan 2 speed is 100% so turn on fan 2 constantly
+    // user fan 2 speed is 100% so turn on fan 2 constantly
+    else if (fanTwoSpeed == 100) { 
       PORTD |= _BV(PD4); // pin 4 high
       fanTwoTiming = 0;
       //Serial.println(F("f2 on"));
     }
-    else { // else fan 2 speed is 1% to 99% so set the fan 2 speed timing
-      fanTwoTiming = (100 - value) * halfWaveDuration;
+    // else fan 2 speed is 1% to 99% so set the fan 2 speed timing
+    else { 
+      // map speed to max half sine wave duration in uS
+      fanTwoTiming = (100 - fanTwoSpeed) * halfWaveDuration;
       //Serial.print(F("f2 uS:")); Serial.println(fanTwoTiming);
+    }
+  }
+}
+
+void calculateAcFrequency() {
+  if (halfWaveMeasurement == 0)
+    halfWaveMeasurement = micros();
+  else {
+    halfWaveMeasurement = micros() - halfWaveMeasurement;
+    if (halfWaveMeasurement >= 8100 && halfWaveMeasurement <= 8500) {
+      halfWaveDuration = 83;
+      getHalfWaveDuration = false;
+    }
+    else if (halfWaveMeasurement >= 9800 && halfWaveMeasurement <= 10200) {
+      halfWaveDuration = 100;
+      getHalfWaveDuration = false;
+    }
+    else {
+      halfWaveMeasurement = 0;
+    }
+    // just for debugging
+    if (halfWaveDuration != 0) {
+      Serial.print(F("HWD:")); Serial.println(halfWaveDuration);
     }
   }
 }
@@ -85,26 +106,7 @@ void i2cReceiveEvent(const int cnt) {
 void zeroCrosssEvent() {
   // On boot of device, work out the AC half wave duration to see if it is 50Hz or 60Hz.
   if (getHalfWaveDuration) {
-    if (halfWaveMeasurement == 0)
-      halfWaveMeasurement = micros();
-    else {
-      halfWaveMeasurement = micros() - halfWaveMeasurement;
-      if (halfWaveMeasurement >= 8100 && halfWaveMeasurement <= 8500) {
-        halfWaveDuration = 83;
-        getHalfWaveDuration = false;
-      }
-      else if (halfWaveMeasurement >= 9800 && halfWaveMeasurement <= 10200) {
-        halfWaveDuration = 100;
-        getHalfWaveDuration = false;
-      }
-      else {
-        halfWaveMeasurement = 0;
-      }
-      // just for debugging
-      if (halfWaveDuration != 0) {
-        Serial.print(F("HWD:")); Serial.println(halfWaveDuration);
-      }
-    }
+    calculateAcFrequency();
   }
   // Else set the AC cutoff peroid for fan one and fan two.
   else {
